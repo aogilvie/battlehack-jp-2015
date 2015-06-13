cfa0edb2381ab0005449b59dc39ef35e8a9572e8
Ally Ogilvie <alex.ogilvie@gmail.com>
2015-06-13 12:11:50 +0900

    accidentally commited random build file, lame

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -rf mobile/www/
 cp -R source mobile/www/
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 
 ./node_modules/bower/bin/bower install ionic
 ./node_modules/cordova/bin/cordova create mobile
+
 pushd mobile
 ../node_modules/cordova/bin/cordova platform add ios android browser
+../node_modules/cordova/bin/cordova plugin add com.paypal.cordova.mobilesdk
+../node_modules/cordova/bin/cordova plugin add org.apache.cordova.console
+
 popd
